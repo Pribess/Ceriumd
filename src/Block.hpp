6e064d08cfa6adf67f437764cdb1ea2f9a0a9680
Pribess <heewon6752@naver.com>
2021-05-07 15:50:18 +0900

    ADD HEADER

--- src/Block.hpp
+++ src/Block.hpp
@@ -1,7 +1,12 @@
+//Copyright (c) 2021 Heewon Cho
+
 class Block
 {
 private:
     unsigned int BlockVersion;
+    char PreviousHash[32];
+    char RootHash[32];
+    unsigned int TimeStamp;
 public:
     Block(unsigned int BlockVersion);
 };  

b31b49c14ca958b356a8b5cd6d37ad5ed68d9ea5
Cho Heewon <heewon@FreeBSD12.2>
2021-05-10 17:20:13 +0900

    FIX

--- src/Ceriumd.cpp
+++ src/Ceriumd.cpp
@@ -4,7 +4,6 @@
 #include "crypto/Crypto.hpp"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, Cerium!" << std::endl;
     std::cout << "   ___          _                 \n  / __\\___ _ __(_)_   _ _ __ ___  \n / /  / _ \\ '__| | | | | '_ ` _ \\\n/ /__|  __/ |  | | |_| | | | | | |\n\\____/\\___|_|  |_|\\__,_|_| |_| |_|" << std::endl;
     std::cout << Crypto::SHA256("hello") << std::endl;
     std::cout << Crypto::SHA256("Hello") << std::endl;

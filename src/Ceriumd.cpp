3d97ce97845a1e0055f070ac909ca3fdf48a8cc0
Pribess <heewon6752@naver.com>
2021-05-10 17:44:01 +0900

    FIX

--- src/Ceriumd.cpp
+++ src/Ceriumd.cpp
@@ -5,7 +5,6 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "   ___          _                 \n  / __\\___ _ __(_)_   _ _ __ ___  \n / /  / _ \\ '__| | | | | '_ ` _ \\\n/ /__|  __/ |  | | |_| | | | | | |\n\\____/\\___|_|  |_|\\__,_|_| |_| |_|" << std::endl;
-    std::cout << Crypto::SHA256("hello") << std::endl;
-    std::cout << Crypto::SHA256("Hello") << std::endl;
+    std::cout << Crypto::SHA256("Hello, Cerium!") << std::endl;
     return 0;
 }

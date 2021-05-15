780ef67cd5b30987a5759021550d0b151b4eece9
Pribess <heewon6752@naver.com>
2021-05-15 15:31:34 +0900

    FIX

--- src/Ceriumd.cpp
+++ src/Ceriumd.cpp
@@ -8,11 +8,8 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "   ___          _                 \n  / __\\___ _ __(_)_   _ _ __ ___  \n / /  / _ \\ '__| | | | | '_ ` _ \\\n/ /__|  __/ |  | | |_| | | | | | |\n\\____/\\___|_|  |_|\\__,_|_| |_| |_|" << std::endl;
-    Block *b1 = new Block(1, "1", "1", 1, 1, 1);
+    Block *b1 = new Block(1, (char *)"1", (char *)"1", 1, 1, 1);
     std::cout << b1->CalculateBlockHash() << std::endl;
     free(b1);
-    Block *b2 = new Block(3, "2", "1", 1, 1, 1);
-    std::cout << b2->CalculateBlockHash() << std::endl;
-    free(b2);
     return 0;
 }

154b760239462089287fb6ca4f8324cf49156b04
Pribess <heewon6752@naver.com>
2021-05-19 00:10:27 +0900

    ADD CONSTRUCTOR SOCKET

--- src/net/Socket.cpp
+++ src/net/Socket.cpp
@@ -1,1 +1,18 @@
 //Copyright (c) 2021 Heewon Cho
+
+#include "Socket.hpp"
+
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+Socket::Socket() {
+    memset(&this->sin, 0, sizeof(this->sin));
+}
+
+Socket::Socket(int sin_family, int sin_port, std::string sin_addr) {
+    memset(&this->sin, 0, sizeof(this->sin));
+    this->sin.sin_family = sin_family;
+    this->sin.sin_port = htons(sin_port);
+    this->sin.sin_addr.s_addr = inet_addr(sin_addr.c_str());
+}
--- src/net/Socket.hpp
+++ src/net/Socket.hpp
@@ -1,9 +1,14 @@
 //Copyright (c) 2021 Heewon Cho
 
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
 
 class Socket
 {
 private:
+    sockaddr_in sin;
 public:
+    Socket();
+    Socket(int sin_family, int sin_port, std::string sin_addr);
 };

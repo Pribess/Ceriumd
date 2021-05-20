754e1aaf35a508a1ece23e4bec716d52ca213d70
Pribess <heewon6752@naver.com>
2021-05-20 11:57:48 +0900

    FIX

--- src/Ceriumd.cpp
+++ src/Ceriumd.cpp
@@ -9,6 +9,7 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
+    char dt[] = "HELLOW";
     std::cout << "   ___          _                 \n  / __\\___ _ __(_)_   _ _ __ ___  \n / /  / _ \\ '__| | | | | '_ ` _ \\\n/ /__|  __/ |  | | |_| | | | | | |\n\\____/\\___|_|  |_|\\__,_|_| |_| |_|" << std::endl;
     Block *b1 = new Block(1, (char *)"1", (char *)"1", 1, 1, 1);
     std::cout << b1->CalculateBlockHash() << std::endl;
@@ -18,5 +19,6 @@ int main(int argc, char* argv[]) {
     s1->BindSocket();
     s1->Listen();
     s1->CloseSocket();
+    delete s1;
     return 0;
 }

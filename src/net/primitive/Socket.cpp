4deeb1784555d13a209a09cf01d3016a912dff07
Pribess <heewon6752@naver.com>
2021-05-24 16:53:01 +0900

    ADD SOCKET HANDLER

---
+++ src/net/primitive/Socket.cpp
@@ -0,0 +1,67 @@
+//Copyright (c) 2021 Heewon Cho
+
+#include "Socket.hpp"
+
+Socket::Socket(int SocketDesc) {
+    this->SocketDesc = SocketDesc;
+}
+
+Socket::~Socket() {
+    this->CloseSocket();
+}
+
+int Socket::CloseSocket() {
+        if (0 > close(this->SocketDesc)) {
+        return -1;
+    }
+    return 0;
+}
+
+int Socket::SendData(int data) {
+    char cnt =5;
+    while (cnt) {
+        if (0 > write(this->SocketDesc, &data, sizeof(data))) {
+            cnt--;
+        } else {
+            return 0;
+        }
+    }
+    throw std::ios_base::failure("Write Data Failed!");
+}
+
+int Socket::SendData(char data[]) {
+    char cnt = 5;
+    while (cnt) {
+        if (0 > write(this->SocketDesc, data, sizeof(&data))) {
+            cnt--;
+        } else {
+            return 0;
+        }
+    }
+    throw std::ios_base::failure("Write Data Failed!");
+}
+
+int Socket::SendData(std::string data) {
+    char cnt = 5;
+    while (cnt) {
+        if (0 > write(this->SocketDesc, data.c_str(), sizeof(data))) {
+            cnt--;
+        } else {
+            return 0;
+        }
+    }
+    throw std::ios_base::failure("Write Data Failed!");
+}
+
+char *Socket::RecvData() {
+    char cnt = 5;
+    memset(this->Buff, 0, sizeof(this->Buff));
+    while (cnt) {
+        if (0 > read(this->SocketDesc, this->Buff, sizeof(this->Buff))) {
+            cnt--;
+        } else {
+            return this->Buff;
+        }
+    }
+    throw std::ios_base::failure("Recieve Data Failed!");
+}
---
+++ src/net/primitive/Socket.hpp
@@ -0,0 +1,21 @@
+//Copyright (c) 2021 Heewon Cho
+
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <unistd.h>
+
+class Socket {
+private:
+    int SocketDesc;
+    char Buff[256];
+public:
+    Socket(int SocketDesc);
+    ~Socket();
+    int CloseSocket();
+    int SendData(int data);
+    int SendData(char data[]);
+    int SendData(std::string data);
+    char *RecvData();
+};

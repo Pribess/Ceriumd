aa5851a890a8c7f236586fea246a02c0c91bf311
Pribess <heewon6752@naver.com>
2021-05-24 22:54:21 +0900

    RESTORE CASTINGTOOLS

---
+++ src/tools/CastingTools.hpp
@@ -0,0 +1,9 @@
+//Copyright (c) 2021 Heewon Cho
+
+class CastingTools {
+private:
+public:
+    static const char *itoa(int Buff);
+    static const char *ctoh(const char *Buff);
+    static const char *ctod(const char *Buff);
+};

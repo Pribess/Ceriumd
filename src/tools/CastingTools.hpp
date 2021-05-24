89e6cad7cf87229d0c7a5684421a05518bb05a24
pribess <heewon6752@naver.com>
2021-05-24 13:26:27 +0000

    RM

--- src/tools/CastingTools.hpp
+++
@@ -1,9 +0,0 @@
-//Copyright (c) 2021 Heewon Cho
-
-class CastingTools {
-private:
-public:
-    static const char *itoa(int Buff);
-    static const char *ctoh(const char *Buff);
-    static const char *ctod(const char *Buff);
-};

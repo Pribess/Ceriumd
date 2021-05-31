386da27d8386f7683893334d24a6650e295f8c3e
Pribess <heewon6752@naver.com>
2021-05-31 18:17:26 +0900

    ADD STRUCTURES

---
+++ src/net/protocol/NetByte.hpp
@@ -0,0 +1,27 @@
+//Copyright (c) 2021 Heewon Cho
+
+#ifndef _CERIUM_NET_PROTOCOL_NETBYTE
+#define _CERIUM_NET_PROTOCOL_NETBYTE
+
+#include "constant/Define.hpp"
+
+namespace NetByte {
+
+    typedef struct {
+        const short magic = CERIUM_PACKET_MAGIC;
+        short type;
+        short length;
+        long checksum;
+    } packetheader;
+
+    typedef struct {
+        unsigned int version;
+        long int timestamp;
+    } version;
+
+    typedef struct {
+
+    } addr;
+}
+
+#endif
---
+++ src/net/protocol/Protocol.hpp
@@ -0,0 +1,11 @@
+//Copyright (c) 2021 Heewon Cho
+
+#ifndef _CERIUM_NET_PROTOCOL_PROTOCOL
+#define _CERIUM_NET_PROTOCOL_PROTOCOL
+
+class Protocol {
+    public:
+        int Version();
+};
+
+#endif

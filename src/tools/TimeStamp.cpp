b28b5a08d2a7d553cb0261eb5fac9dcc6d07a7b7
Pribess <heewon6752@naver.com>
2021-06-01 15:41:32 +0900

    ADD TIMESTAMP

---
+++ src/tools/TimeStamp.cpp
@@ -0,0 +1,7 @@
+//Copyright (c) 2021 Heewon Cho
+
+#include "TimeStamp.hpp"
+
+long int TimeStamp::GetUtcTimeStamp() {
+    return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
---
+++ src/tools/TimeStamp.hpp
@@ -0,0 +1,13 @@
+//Copyright (c) 2021 Heewon Cho
+
+#ifndef _CERIUM_TOOLS_TIMESTAMP
+#define _CERIUM_TOOLS_TIMESTAMP
+
+#include <chrono>
+
+class TimeStamp {
+    public:
+        static long int GetUtcTimeStamp();
+};
+
+#endif

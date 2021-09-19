ce85bfe5e9543b9c5bf89f02bc7c23f95b8878fe
Pribess <heewon6752@naver.com>
2021-09-20 02:26:42 +0900

    ALTER BUILDSYSTEM TO CMAKE

---
+++ build/CMakeFiles/ceriumd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ceriumd.dir/src/Ceriumd.cpp.o"
+  "CMakeFiles/ceriumd.dir/src/Ceriumd.cpp.o.d"
+  "ceriumd"
+  "ceriumd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ceriumd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
